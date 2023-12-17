2d452526c8bdf78f4bdc897a2c0ff7145da2d23c
Domains18 <gibsonsgibson88@gmail.com>
2023-12-17 20:25:09 +0300

    cargo and hello world

---
+++ hello_world/target/debug/deps/hello_world-8e894cd653037ef5.d
@@ -0,0 +1,5 @@
+/home/alpha/Documents/Github/Rust/hello_world/target/debug/deps/hello_world-8e894cd653037ef5: src/main.rs
+
+/home/alpha/Documents/Github/Rust/hello_world/target/debug/deps/hello_world-8e894cd653037ef5.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/alpha/Documents/Github/Rust/hello_world/target/debug/hello_world: /home/alpha/Documents/Github/Rust/hello_world/src/main.rs
