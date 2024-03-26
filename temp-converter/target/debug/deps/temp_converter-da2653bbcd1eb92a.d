8c0304561f9c67a3843ec0854bb53e5c20b2a704
Domains18 <gibsonsgibson88@gmail.com>
2024-03-26 17:37:52 +0300

    temp covertor

---
+++ temp-converter/target/debug/deps/temp_converter-da2653bbcd1eb92a.d
@@ -0,0 +1,5 @@
+/Users/alphauser/Documents/Github/rust/Rustlings/temp-converter/target/debug/deps/temp_converter-da2653bbcd1eb92a: src/main.rs
+
+/Users/alphauser/Documents/Github/rust/Rustlings/temp-converter/target/debug/deps/temp_converter-da2653bbcd1eb92a.d: src/main.rs
+
+src/main.rs:
---
+++ temp-converter/target/debug/temp-converter.d
@@ -0,0 +1,1 @@
+/Users/alphauser/Documents/Github/rust/Rustlings/temp-converter/target/debug/temp-converter: /Users/alphauser/Documents/Github/rust/Rustlings/temp-converter/src/main.rs
