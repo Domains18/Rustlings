c41dbb80a867271e1906bf0c5ee262af6bfc0184
Domains18 <gibsonsgibson88@gmail.com>
2025-07-17 22:50:38 +0300

    feat (todolist) added code for looping a cli

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+extern crate todo_list;
+use todo_list::Task;
+
+
+use std::io::stdin;
+use std::io::stdout;
+use std::io::Write;
+
+
+
+fn runprompt(todo: &mut Vec<Task>){
+    loop {
+        let mut stdout = stdout();
+        print!("(todo list) > ");
+        stdout.flush().expect("can't flush the stdout");
+
+        let mut buffer = String::new();
+        stdin().read_line(&mut buffer).expect("cannot readline");
+
+        let args: Vec<&str> = buffer.split_whitespace().collect();
+
+        todo_list::run(args, todo);
+    }
+}
+
+fn main(){
+    let mut todo: Vec<Task> = Vec::new();
+
+    runprompt(&mut todo);
 }
